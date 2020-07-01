5830260fb9f88a813e877ed9eb64aa46f78499fc
roykimm <roykimmyunghoon@gmail.com>
2020-07-01 23:43:23 +0900

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = 7020;
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req,res) => {
+    res.send('./public/index.html');
+})
+
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
